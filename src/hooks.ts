326aced612bee9440e5a97a516ab587d1376f305
CRIMX <straybugs@gmail.com>
2023-07-22 21:02:09 +0800

    refactor: add typedoc

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,6 +4,9 @@ import { I18nContext } from "./i18n-context";
 
 import type { I18n, LocaleLang, TFunction } from "val-i18n";
 
+/**
+ * @returns The {@link I18n} instance from the nearest {@link I18nProvider}. Throws if not found.
+ */
 export const useI18n = (): I18n => {
   const i18n = useContext(I18nContext);
   if (!i18n) {
@@ -12,6 +15,12 @@ export const useI18n = (): I18n => {
   return i18n;
 };
 
+/**
+ * @returns The {@link TFunction} from the nearest {@link I18nProvider}. Throws if not found.
+ */
 export const useTranslate = (): TFunction => useVal(useI18n().t$);
 
+/**
+ * @returns The {@link LocaleLang} from the nearest {@link I18nProvider}. Throws if not found.
+ */
 export const useLang = (): LocaleLang => useVal(useI18n().lang$);
--- src/i18n-context.ts
+++ src/i18n-context.ts
@@ -7,5 +7,8 @@ export const I18nContext = /* @__PURE__ */ createContext<I18n | null>(null);
 
 export type I18nProviderProps = PropsWithChildren<{ i18n: I18n }>;
 
+/**
+ * Provides the I18n instance to the rest of the app.
+ */
 export const I18nProvider: FC<I18nProviderProps> = ({ i18n, children }) =>
   createElement(I18nContext.Provider, { value: i18n, children });
