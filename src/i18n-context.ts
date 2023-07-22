08104d6c15087618ff3859748bb4e7afe61fcdd8
CRIMX <straybugs@gmail.com>
2023-07-22 21:34:41 +0800

    refactor: update I18nProvider typing

--- src/i18n-context.ts
+++ src/i18n-context.ts
@@ -5,10 +5,20 @@ import { createContext, createElement } from "react";
 
 export const I18nContext = /* @__PURE__ */ createContext<I18n | null>(null);
 
-export type I18nProviderProps = PropsWithChildren<{ i18n: I18n }>;
+export interface I18nProviderProps {
+  i18n: I18n;
+}
 
 /**
  * Provides the I18n instance to the rest of the app.
+ *
+ * @example
+ * ```tsx
+ * <I18nProvider i18n={i18n}>
+ *   <App />
+ * </I18nProvider>
  */
-export const I18nProvider: FC<I18nProviderProps> = ({ i18n, children }) =>
-  createElement(I18nContext.Provider, { value: i18n, children });
+export const I18nProvider: FC<PropsWithChildren<I18nProviderProps>> = ({
+  i18n,
+  children,
+}) => createElement(I18nContext.Provider, { value: i18n, children });
