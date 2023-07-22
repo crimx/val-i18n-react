da2025ae740e3f4c4482343c48b213d04f197247
CRIMX <straybugs@gmail.com>
2023-07-22 12:34:34 +0800

    refactor: use FC typing

--- src/i18n-context.ts
+++ src/i18n-context.ts
@@ -1,4 +1,4 @@
-import type { PropsWithChildren } from "react";
+import type { FC, PropsWithChildren } from "react";
 import type { I18n } from "val-i18n";
 
 import { createContext, createElement } from "react";
@@ -7,5 +7,5 @@ export const I18nContext = /* @__PURE__ */ createContext<I18n | null>(null);
 
 export type I18nProviderProps = PropsWithChildren<{ i18n: I18n }>;
 
-export const I18nProvider = ({ i18n, children }: I18nProviderProps) =>
+export const I18nProvider: FC<I18nProviderProps> = ({ i18n, children }) =>
   createElement(I18nContext.Provider, { value: i18n, children });
